0b4a59c67b19a8daf2df404949e29db5ed2639a0
jaszczompeu <deathlineking@gmail.com>
2019-03-26 21:54:18 +0100

    small changes

--- js.js
+++ js.js
@@ -8,6 +8,7 @@ $(function(){
     var cenawie = 0.35;
     var cenaw = 0;
     var cenak = 0;
+    var iloscsztuk = 0;
 
     $('.podlicz').click(function(){
         var utnijszer = "(Utnij:"+(($('.szer').val())-39)+" )";
@@ -27,6 +28,7 @@ $(function(){
         var cenabrutto = (((obw*cenapro)+(4*cenanar)+((szer*wys)*cenasia)+(8*cenazac)+(8*cenapod)+cenawie+(obw*cenausz)+poprzeczka)*0.85)*1.23;
         cenabrutto=cenabrutto*ile;
         dlaklienta=cenabrutto*3;
+        iloscsztuk += ile;
         dotabelki(szer.toFixed(3),utnijwys,wys.toFixed(3),utnijszer,ile,cenabrutto.toFixed(2),dlaklienta.toFixed(2));
     });
 
@@ -37,8 +39,10 @@ $(function(){
     $(document).on('click', '#body', function(){
         var ck = $('.ck');
         var cw = $('.cw');
+        var il = $('.il')
         cenaw = 0;
         cenak = 0;
+        ilesz = 0;
 
         $(".ck").each(function() {
             var value = $(this).text();
@@ -54,15 +58,52 @@ $(function(){
                 cenaw += parseFloat(value);
             }
         });
+        $(".il").each(function() {
+            var value = $(this).text();
+            // add only if the value is number
+            if(!isNaN(value) && value.length != 0) {
+                ilesz += parseFloat(value);
+            }
+        });
 
-
-        $('.cenawytw').text(cenaw);
-        $('.cenaklient').text(cenak);
+        $('.cenawytw').text(cenaw.toFixed(2));
+        $('.cenaklient').text(cenak.toFixed(2));
+        $('.sztuki').text(ilesz);
     });
+
+    $(document).on('click', '.oferta', createPDF);
 
 });
 
 
 function dotabelki(szerokosc,tnijwys,wysokosc,tnijszer,sztuk,koszt,cena){
-    $('table tr:last').before('<tr><th>'+szerokosc+tnijwys+'</th><th>'+wysokosc+tnijszer+'</th><th>'+sztuk+'</th><th class="cw">'+koszt+'</th><th class="ck">'+cena+'</th><th><input type="button" class="delete" value="x"></th></tr>');
+    $('table tr:last').before('<tr><th>'+szerokosc+tnijszer+'</th><th>'+wysokosc+tnijwys+'</th><th class="il">'+sztuk+'</th><th class="cw">'+koszt+'</th><th class="ck">'+cena+'</th><th><input type="button" class="delete" value="x"></th></tr>');
 }
+/*
+function createPDF(){
+    var doc = new jsPDF();
+    var doclineheightx = 20;
+    var doclineheighty = 190;
+
+    $('tr').each(function(){
+        //adding line for every tr
+        doc.line(20,doclineheightx,190,doclineheightx,190,20);
+        doc.setLineWidth(0.5);
+        doclineheightx+=20;
+        doclineheighty+=20;
+
+        //first cell and lines
+
+        doc.text 
+
+        
+        
+
+    });
+
+    
+    
+    doc.save('oferta.pdf');
+   
+}
+*/ 
